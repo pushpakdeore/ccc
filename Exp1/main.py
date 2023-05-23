d5e13d483380c2ed8bebbbbf5d34745bab9fed1b
Pushpak Deore <110999787+pushpakdeore@users.noreply.github.com>
2023-05-24 00:05:26 +0530

    Add files via upload

---
+++ Exp1/main.py
@@ -0,0 +1,12 @@
+
+import webapp2
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+    
+        self.response.out.write("Hello world")
+
+    
+
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
